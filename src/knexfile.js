d45b97d4b9ba7fde12cc8cea3807f7e0a271843a
Justin Hays <jhays124@yahoo.com>
2017-11-16 17:47:45 -0800

    setting up config

--- src/knexfile.js
+++ src/knexfile.js
@@ -14,6 +14,6 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: 'https://damp-atoll-31773.herokuapp.com/'
+    connection: 'postgres://douyotilmxtqhj:ec63124b41ffdd6ec9070f599bc9f0b09b4e91b01210f5f464a8f03401ce7b10@ec2-107-21-109-15.compute-1.amazonaws.com:5432/d7pmbilusv113m' + '?ssl=true'
   }
 }
